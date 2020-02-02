7980de553a5504e7eda5e23267ff512a0a811eab
Hayden Perry <hayden@pollen8.io>
2020-02-02 23:26:46 +0000

    Added tests for yank

--- tests/yank.spec.js
+++ tests/yank.spec.js
@@ -9,5 +9,43 @@ describe('src/yank', () => {
       firstName: 'John',
       lastName: 'Doe'
     })
-  }) 
+  })
+
+  it('should ignore all properties that do not exist on the data object', () => {
+    const yanked = yank(data, 'emailAddress', 'firstName')
+
+    expect(yanked).toEqual({
+      firstName: 'John'
+    })
+  })
+
+  it('should return nested results', () => {
+    const yanked = yank(data, 'addressDetails: { city, postcode }')
+
+    expect(yanked).toEqual({
+      addressDetails: {
+        city: 'London',
+        postcode: 'SW1A 2AB'
+      }
+    })
+  })
+
+  it('should return renamed keys that are separated by ->', () => {
+    const yanked = yank(data, 'firstName->first_name', 'lastName->last_name')
+
+    expect(yanked).toEqual({
+      first_name: 'John',
+      last_name: 'Doe'
+    })
+  })
+
+  it('should throw if any of the arguments after the first are not strings', done => {
+    try {
+      yank(data, 'firstName', 1, true)
+    } catch (e) {
+      expect(e).toEqual('All arguments must be strings')
+
+      done()
+    }
+  })
 })
